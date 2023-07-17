213dedf231cc8f3eaf784cefc3459980b96e08ef
Christoph Kralj <christoph.kralj@gmail.com>
2023-07-17 15:39:47 +0200

    Solved day2 part 1

---
+++ day2_1/src/main.rs
@@ -0,0 +1,83 @@
+use itertools::Itertools;
+use std::{collections::HashMap, result};
+
+#[derive(Debug, PartialEq)]
+enum Action {
+    Rock,
+    Paper,
+    Scissor,
+}
+
+#[derive(Debug)]
+struct Game {
+    ours: Action,
+    theirs: Action,
+}
+
+impl Game {
+    fn from(input: &str) -> Option<Self> {
+        let preprocessed_input = input.trim();
+        if preprocessed_input.len() == 3
+            && input.contains(" ")
+            && input.contains(['A', 'B', 'C'])
+            && input.contains(['X', 'Y', 'Z'])
+        {
+            if let Some((t, o)) = preprocessed_input.split(" ").collect_tuple() {
+                Some(Self {
+                    ours: string_to_action(o),
+                    theirs: string_to_action(t),
+                })
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+
+    fn score(&self) -> u32 {
+        if self.ours == self.theirs {
+            action_to_value(&self.ours) + 3
+        } else if ours_winning(&self.ours, &self.theirs) {
+            action_to_value(&self.ours) + 6
+        } else {
+            action_to_value(&self.ours)
+        }
+    }
+}
+
+fn ours_winning(ours: &Action, theirs: &Action) -> bool {
+    (*ours == Action::Paper && *theirs == Action::Rock)
+        || (*ours == Action::Rock && *theirs == Action::Scissor)
+        || (*ours == Action::Scissor && *theirs == Action::Paper)
+}
+
+fn action_to_value(action: &Action) -> u32 {
+    match action {
+        Action::Rock => 1,
+        Action::Paper => 2,
+        Action::Scissor => 3,
+    }
+}
+
+fn string_to_action(input: &str) -> Action {
+    match input {
+        "A" => Action::Rock,
+        "X" => Action::Rock,
+        "B" => Action::Paper,
+        "Y" => Action::Paper,
+        "C" => Action::Scissor,
+        "Z" => Action::Scissor,
+        _ => panic!("Error during line parsing: {}", input),
+    }
+}
+
+fn main() {
+    let result: u32 = include_str!("input.txt")
+        .lines()
+        .map(Game::from)
+        .filter_map(|g| Some(g?.score()))
+        .sum();
+
+    println!("{:?}", result)
+}
